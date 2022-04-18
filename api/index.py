56dc95f8c05681106cccdd2a877ef9809e77045f
dco <uiqian@live.cn>
2022-04-18 13:34:21 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ connect(host="mongodb+srv://nodebb:9rGHFMefugTeASfF@cluster0.ntvar.mongodb.net/D
 class Users(Document):
     email = StringField(max_length=200, required=True)
     tk = StringField()
-    date_modified = DateTimeField(default=datetime.datetime.utcnow)
+    date_modified = DateTimeField(default=datetime.datetime.now)
 
 
 user1 = Users(
