02349539237b71c690e3987276cafa3590f39283
dco <uiqian@live.cn>
2022-04-18 22:57:39 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from mongoengine import *
 import datetime
+import hashlib
 
 connect(host="mongodb+srv://nodebb:9rGHFMefugTeASfF@cluster0.ntvar.mongodb.net/DCOCD?retryWrites=true&w=majority")
 
@@ -9,7 +10,11 @@ class Users(Document):
     tk = StringField()
     update_time = DateTimeField(default=datetime.datetime.utcnow()+datetime.timedelta(hours=8))
 
-user = Users()
+def strMD5(source):
+    md5 = hashlib.md5()
+    source = source + 'dcocd.com'
+    md5.update(source.encode('utf-8'))
+    return md5.hexdigest()
 
 app = Flask(__name__)
 
@@ -22,29 +27,25 @@ def hello():
 @app.route('/reg', methods=['POST'])
 def reg():
     user = Users.objects.filter(email=request.form['email'])
+    print(user)
     if user:
-        user.email = request.form['email']
+        user=user[0]
         user.update_time = datetime.datetime.utcnow()+datetime.timedelta(hours=8)
-        user.tk = 'testmd5'
-        user.save()
-        return 'is not exist!'
-    else:
-        user.update_time = datetime.datetime.utcnow()+datetime.timedelta(hours=8)
-        user.tk = 'is exists'
+        user.tk = strMD5(user.email+user.update_time)
         user.save()
         return 'is exists'
+
+    else:
+        user = Users()
+        user.email = request.form['email']
+        user.update_time = datetime.datetime.utcnow()+datetime.timedelta(hours=8)
+        user.tk = strMD5(request.form['email']+user.update_time)
+        user.save()
+        return 'is not exist!'
         
-
-    '''
-    user.email = request.form['email']
-    user.update_time = datetime.datetime.utcnow()+datetime.timedelta(hours=8)
-    user.tk = 'testmd5'
-    user.save()
-    return 'adcpost'
-    '''
-
 @app.route('/result')
 def result():
    dict = {'phy':50,'che':60,'maths':70}
    return render_template('result.html', result = dict)
 
+#app.run()
