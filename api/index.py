6a66771022195087dede00799f2c27b6b4080182
dco <uiqian@live.cn>
2022-04-18 13:09:03 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template
+from mongoengine import *
+import datetime
+
+connect(host="mongodb+srv://nodebb:9rGHFMefugTeASfF@cluster0.ntvar.mongodb.net/DCOCD?retryWrites=true&w=majority")
+
+class Users(Document):
+    email = StringField(max_length=200, required=True)
+    tk = StringField()
+    date_modified = DateTimeField(default=datetime.datetime.utcnow)
+
+
+user1 = Users(
+    email = 'test@test.com',
+    tk = 'abcdefg'
+    )
+
+user1.save()
+print(user1.email)
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello():
     return 'Hello, world'
 
-
 @app.route('/test')
 def test():
-    return 'Test'
+    return user1.email
 
 @app.route('/result')
 def result():
